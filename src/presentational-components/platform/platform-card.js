e38761201bc0cd8a443bed473561317e3e36281c
Laura Galis <lgalis@redhat.com>
2019-12-09 10:28:01 -0500

    Fixed lint errors

--- src/presentational-components/platform/platform-card.js
+++ src/presentational-components/platform/platform-card.js
@@ -1,6 +1,6 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
-import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   Card,
   CardHeader,
