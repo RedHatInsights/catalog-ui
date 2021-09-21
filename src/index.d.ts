611fe9ec27998f15e676fc998367a510de5cd2da
Laura Galis <lgalis@redhat.com>
2021-09-21 14:12:02 -0400

    Update portfolio items APIs

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,3 @@
-// Fool TypeScript into thinking that we actually have typings for these components.
-// This will tell typescript that anything from this module is of type any.
-
-declare module 'react-markdown';
-declare module 'react-router-hash-link';
-declare module 'file-saver';
-declare module '*.gif';
-declare module '*.svg';
-
 // Declare configuration globals here so that TypeScript compiles
 declare let API_HOST;
 declare let API_BASE_PATH;
