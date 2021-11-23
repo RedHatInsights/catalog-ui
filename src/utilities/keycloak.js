316ac535ace4c46dde92acb10efc62077c0719b7
Laura Galis <lgalis@redhat.com>
2021-11-23 14:51:16 -0500

    Add keycloak authentication

--- src/utilities/keycloak.js
+++ src/utilities/keycloak.js
@@ -1,12 +1,57 @@
 import Keycloak from 'keycloak-js';
 
-const keycloakConfig = {
-  url: 'http://catalog.k8s.local/login/keycloak-oidc',
-  realm: 'Aap',
-  clientId: 'catalog'
+const _keycloak = new Keycloak('./keycloak.json');
+
+/**
+ * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
+ *
+ * @param onAuthenticatedCallback
+ */
+const initKeycloak = (onAuthenticatedCallback) => {
+  _keycloak
+    .init({
+      onLoad: 'check-sso',
+      silentCheckSsoRedirectUri:
+        window.location.origin + '/silent-check-sso.html',
+      pkceMethod: 'S256'
+    })
+    .then((authenticated) => {
+      if (!authenticated) {
+        console.log('user is not authenticated..!');
+      }
+
+      onAuthenticatedCallback();
+    })
+    .catch(console.error);
 };
 
-const keycloak = new Keycloak(keycloakConfig);
-console.log('Keycloak: ', keycloak);
+const doLogin = _keycloak.login;
 
-export default keycloak;
+const doLogout = _keycloak.logout;
+
+const getToken = () => _keycloak.token;
+
+const isLoggedIn = () => !!_keycloak.token;
+
+const updateToken = (successCallback) =>
+  _keycloak
+    .updateToken(5)
+    .then(successCallback)
+    .catch(doLogin);
+
+const getUsername = () => _keycloak.tokenParsed?.preferred_username;
+
+const hasRole = (roles) => roles.some((role) => _keycloak.hasRealmRole(role));
+
+const CatalogAuth = {
+  initKeycloak,
+  doLogin,
+  doLogout,
+  isLoggedIn,
+  getToken,
+  updateToken,
+  getUsername,
+  hasRole
+};
+
+export default CatalogAuth;
